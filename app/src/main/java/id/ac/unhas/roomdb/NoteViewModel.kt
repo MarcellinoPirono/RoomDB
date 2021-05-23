a053247ef7896754ed90da12d68a608dce334e2e
MarcellinoPirono <marcellinopirono19@gmail.com>
2021-05-23 18:31:36 +0800

    View Model

---
+++ app/src/main/java/id/ac/unhas/roomdb/NoteViewModel.kt
@@ -0,0 +1,30 @@
+package id.ac.unhas.roomdb
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import id.ac.unhas.roomdb.Note
+import id.ac.unhas.roomdb.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var noteRepository = NoteRepository(application)
+    private var notes: LiveData<List<Note>>? = noteRepository.getNotes()
+
+    fun insertNote(note: Note) {
+        noteRepository.insert(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>>? {
+        return notes
+    }
+
+    fun deleteNote(note: Note) {
+        noteRepository.delete(note)
+    }
+
+    fun updateNote(note: Note) {
+        noteRepository.update(note)
+    }
+
+}
