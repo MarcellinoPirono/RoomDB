b75d453fc51627a1a462606128e9528109b9cb71
MarcellinoPirono <marcellinopirono19@gmail.com>
2021-05-23 18:22:32 +0800

    Dao (NoteDao)

--- app/src/main/java/id/ac/unhas/roomdb/AppDatabase.kt
+++ app/src/main/java/id/ac/unhas/roomdb/AppDatabase.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import id.ac.unhas.roomdb.note.Note
-import id.ac.unhas.roomdb.note.NoteDao
+import id.ac.unhas.roomdb.Note
+import id.ac.unhas.roomdb.NoteDao
 
 @Database(entities = [Note::class], exportSchema = false, version = 1)
 abstract class AppDatabase : RoomDatabase() {
